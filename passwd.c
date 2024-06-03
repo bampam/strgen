dbbfab9ab022c991b8e5d2875d9664a664c7ff61
bampham <cbroo@duck.com>
2024-06-03 15:55:34 +0200

    custom seed

--- passwd.c
+++ passwd.c
@@ -10,16 +10,33 @@ const char lowercase[] = "abcdefghijklmnopqrstuvwxyz";
 const char numbers[] = "0123456789";
 const char symbols[] = "!@#$%^&*()_-=+{[]}\\|;:'\",<.>/?";
 
+typedef struct {
+    char av_chars[256];
+    int total_chars;
+    int password_length;
+    int use_clipboard;
+    int display_password;
+    int file_index;
+    int has_custom_chars;
+    int display_seed;
+    int use_custom_seed;
+} Passwd;
+
+void init_passwd(Passwd* passwd) {
+    passwd->av_chars[0] = '\0';
+    passwd->total_chars = 0;
+    passwd->password_length = 8;
+    passwd->use_clipboard = 0;
+    passwd->display_password = 1;
+    passwd->file_index = 0;
+    passwd->has_custom_chars = 0;
+    passwd->display_seed = 0;
+    passwd->use_custom_seed = 0;
+}
+
 void generate_password(int argc, char** argv) {
-    srand(time(0));
-    int length = 8;
-    char chars[128] = "";
-    int chars_length = 0;
-    int clipboard = 0;
-    int noprint = 0;
-    int fout = 0;
-    int fname = 0;
-    int custom_chars = 0;
+    Passwd passwd;
+    init_passwd(&passwd);
 
     if (argc == 1) {
         goto generate;
@@ -29,51 +46,58 @@ void generate_password(int argc, char** argv) {
     for (int i = 1; i < argc; ++i) {
         // adds uppercase letters
         if (strcmp(argv[i], "-upper") == 0) {
-            strncat(chars, uppercase, sizeof(chars) - strlen(chars) - 1);
-            chars_length += strlen(uppercase);
+            strncat(passwd.av_chars, uppercase, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+            passwd.total_chars += strlen(uppercase);
         }
         // adds lowercase letters
         else if (strcmp(argv[i], "-lower") == 0) {
-            strncat(chars, lowercase, sizeof(chars) - strlen(chars) - 1);
-            chars_length += strlen(lowercase);
+            strncat(passwd.av_chars, lowercase, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+            passwd.total_chars += strlen(lowercase);
         }
         // adds numbers
         else if (strcmp(argv[i], "-num") == 0) {
-            strncat(chars, numbers, sizeof(chars) - strlen(chars) - 1);
-            chars_length += strlen(numbers);
+            strncat(passwd.av_chars, numbers, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+            passwd.total_chars += strlen(numbers);
         }
         // add symbols
         else if (strcmp(argv[i], "-sym") == 0) {
-            strncat(chars, symbols, sizeof(chars) - strlen(chars) - 1);
-            chars_length += strlen(symbols);
+            strncat(passwd.av_chars, symbols, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+            passwd.total_chars += strlen(symbols);
         }
         // sets custom length
         else if (strcmp(argv[i], "-len") == 0) {
-            if (argc > i + 1)
-                length = atoi(argv[++i]);
+            if (argc > i + 1) {
+                passwd.password_length = atoi(argv[++i]);
+            }
         }
         // sets to copy to clipboard
         else if (strcmp(argv[i], "-cb") == 0) {
-            clipboard = 1;
+            passwd.use_clipboard = 1;
         }
         // sets to refrain from printing
         else if (strcmp(argv[i], "-np") == 0) {
-            noprint = 1;
+            passwd.display_password = 0;
         }
+        // specify character to be used 
         else if (strcmp(argv[i], "-ch") == 0) {
             if (argc > i + 1) {
                 ++i;
-                strncat(chars, argv[i], sizeof(chars) - strlen(chars) - 1);
-                custom_chars = 1;
-                chars_length += strlen(argv[i]);
+                strncat(passwd.av_chars, argv[i], sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+                passwd.has_custom_chars = 1;
+                passwd.total_chars += strlen(argv[i]);
+            }
+        }
+        else if (strcmp(argv[i], "-seed") == 0) {
+            if (argc > i + 1) {
+                ++i;
+                passwd.use_custom_seed = atoi(argv[i]);
             }
         }
         // sets to copy to textfile
         else if (strcmp(argv[i], "-o") == 0) {
             if (argc > i + 1) {
-               ++i;
-               fname = i;
-               fout = 1;
+                ++i;
+                passwd.file_index = i;
             }
         }
         else if (strcmp(argv[i], "-h") == 0) {
@@ -82,34 +106,44 @@ void generate_password(int argc, char** argv) {
         }
     }
 
-    if (length == 0) {
-        fprintf(stderr, "Length cannot be 0");
+    if (passwd.password_length == 0) {
+        fprintf(stderr, "Length cannot be 0\n");
         return;
     }
 
 generate:
-    
-    /* if length is 0, all char attributes are added to the password */
-    if (chars_length + custom_chars == 0) {
-        strcat(chars, uppercase);
-        strcat(chars, lowercase);
-        strcat(chars, numbers);
-        strcat(chars, symbols);
-        chars_length = strlen(uppercase) + strlen(lowercase) + strlen(numbers) + strlen(symbols);
+    /* if no characters are specified, use all character attributes */
+    if (passwd.total_chars == 0) {
+        strncat(passwd.av_chars, uppercase, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+        strncat(passwd.av_chars, lowercase, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+        strncat(passwd.av_chars, numbers, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+        strncat(passwd.av_chars, symbols, sizeof(passwd.av_chars) - strlen(passwd.av_chars) - 1);
+        passwd.total_chars = strlen(passwd.av_chars);
     }
 
-    char password[length + 1];
-    for (int i = 0; i < length; ++i) {
-        password[i] = chars[(rand() % chars_length)];
+    // set seed to either custom or unix-time
+    if (passwd.use_custom_seed > 0) {
+        srand(passwd.use_custom_seed);
+    } else {
+        srand(time(0));
     }
 
-    password[length] = '\0';
-    if (clipboard > 0)
+    /* Generate password */
+    char password[passwd.password_length + 1];
+    for (int i = 0; i < passwd.password_length; ++i) {
+        password[i] = passwd.av_chars[rand() % passwd.total_chars];
+    }
+
+    password[passwd.password_length] = '\0';
+    if (passwd.use_clipboard > 0) {
         to_clipboard(password);
+    }
 
-    if (fout > 0)
-        to_file(password, argv[fname]);
+    if (passwd.file_index > 0) {
+        to_file(password, argv[passwd.file_index]);
+    }
 
-    if (noprint == 0)
+    if (passwd.display_password == 1) {
         printf("%s\n", password);
+    }
 }
